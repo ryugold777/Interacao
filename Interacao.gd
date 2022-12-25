b1a975ef9cbf103960aa2045fbd4564557ec7243
Ryu777 <76182721+ryugold777@users.noreply.github.com>
2022-12-25 18:50:22 -0300

    Update Interacao.gd

--- Interacao.gd
+++ Interacao.gd
@@ -1,10 +1,25 @@
 extends Area2D
+
+###############################################################################
+# Script para interagir com NPC's e objetos. Caso o personagem "Robin_Erikson"#
+# Entra na área e presione a tecla de confirmar abrirá uma caixa de           #
+# mensagem. O valor da variável "pagina" dever ser sempre zero. O que deve    #
+# mudar é a variável "total_paginas". Caso o personagem saia da área e        #
+# pressionar a tecla de confirmação, não acontecerá nada. O Nó/Node           #
+# "$Imagem_Caixa_Mensagem/Exibir_Mensagem" é RichTextLabel.                   #
+#                                                                             #
+#                                                                             #
+# Autor: Gold Angel                                                           #
+# Data: Dias 5/25 de Dezembro de 2022, 18:45                                  #
+# Agradecimentos/Thanks to a KoBeWi                                           #
+###############################################################################
+
 # Declaração das variáveis
 var pagina = 0
-var totalPaginas = 2
-var proximoTexto
+var total_paginas = 2
+var proximo_texto
 var objeto
-var proximaPagina = true
+var proxima_pagina = true
 var finalizarTexto
                      #página 0               página 1              p
 var mensagem = ["oi tudo bom com voce", "ola ok voce esta aqui", "bom ainda bem"]
@@ -13,60 +28,57 @@ func _ready():
 	$Imagem_Caixa_Mensagem/Exibir_Mensagem.visible = false
 
 # Função mostrar a mensagem
-func mostrarMensagem():
+func mostrar_mensagem():
 	$Imagem_Caixa_Mensagem/Exibir_Mensagem.bbcode_enabled = true
 	$Imagem_Caixa_Mensagem/Exibir_Mensagem.set_bbcode(mensagem[pagina])
 	$Imagem_Caixa_Mensagem/Exibir_Mensagem.show()
 
 # Função esconder a mensagem
-func esconderMensagem():
+func esconder_mensagem():
 	$Imagem_Caixa_Mensagem.visible = false
 	$Imagem_Caixa_Mensagem/Exibir_Mensagem.hide()
 	$Imagem_Caixa_Mensagem/Exibir_Mensagem.visible_characters = 0
 
 # Função interagir com objetos e NPC's
 func interacao():
-	if Input.is_action_just_pressed("confirmar") && proximaPagina == true:
+	if Input.is_action_just_pressed("confirmar") && proxima_pagina == true:
 			$Imagem_Caixa_Mensagem.visible = true
 			pagina = 0
 			while $Imagem_Caixa_Mensagem/Exibir_Mensagem.visible_characters < $Imagem_Caixa_Mensagem/Exibir_Mensagem.text.length():
 				$Imagem_Caixa_Mensagem/Exibir_Mensagem.visible_characters += 1
 				yield(get_tree().create_timer(0.1), "timeout")
-				mostrarMensagem()
+				mostrar_mensagem()
 
-			finalizarTexto = true
-			proximaPagina = false
+			finalizar_texto = true
+			proxima_pagina = false
 
 
 # Próxima página. Para mais páginas acrescentar no topo do script
-	if Input.is_action_just_released("confirmar") && finalizarTexto == true:
+	if Input.is_action_just_released("confirmar") && finalizar_texto == true:
 		if pagina < totalPaginas && $Imagem_Caixa_Mensagem/Exibir_Mensagem.visible_characters >= $Imagem_Caixa_Mensagem/Exibir_Mensagem.text.length():
 			pagina = pagina + 1
 			$Imagem_Caixa_Mensagem/Exibir_Mensagem.visible_characters = -1
 			while $Imagem_Caixa_Mensagem/Exibir_Mensagem.visible_characters < $Imagem_Caixa_Mensagem/Exibir_Mensagem.text.length():
 				$Imagem_Caixa_Mensagem/Exibir_Mensagem.visible_characters += 1
 				yield(get_tree().create_timer(0.1), "timeout")
-				mostrarMensagem()
+				mostrar_mensagem()
 		if pagina >= 1:
-				mostrarMensagem()
+				mostrar_mensagem()
 		if pagina >= totalPaginas && $Imagem_Caixa_Mensagem/Exibir_Mensagem.visible_characters >= $Imagem_Caixa_Mensagem/Exibir_Mensagem.text.length():
-			proximoTexto = true
+			proximo_texto = true
 #Fim da mensagem
-	if Input.is_action_just_released("confirmar") && proximoTexto == true:
-		esconderMensagem()
-		proximoTexto = false
+	if Input.is_action_just_released("confirmar") && proximo_texto == true:
+		esconder_mensagem()
+		proximo_texto = false
 		yield(get_tree().create_timer(0.3), "timeout")
 		$Imagem_Caixa_Mensagem/Exibir_Mensagem.visible_characters =  0
-		proximaPagina = true
-		finalizarTexto = false
+		proxima_pagina = true
+		finalizar_texto = false
 
 # Chamada da função interacao()
 func _process(_delta):
 	interacao()
-#	print($Imagem_Caixa_Mensagem/Exibir_Mensagem.get_total_character_count())
-#	print(mensagem[pagina].length())
-#	print($Imagem_Caixa_Mensagem/Exibir_Mensagem.visible_characters)
-	print(pagina)
+
 # Quando entrar na área
 func entrar_area(body):	
 	if (body.get_name() == "Robin_Erikson"):
